8acdab94f8048eb08647fd59cf25eb2d0f36d0a1
sankeshreddy <sankeshreddy@gmail.com>
2020-08-07 11:51:39 +0530

    tic-tac-toe game implementation using Angular 9

---
+++ tic-tac-toe/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
+//fornt awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//Toaster
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
