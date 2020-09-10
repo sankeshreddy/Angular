da7d11f7d8d40009ca31d4f64d9ceaa5839cdc6a
sankeshreddy <sankeshreddy@gmail.com>
2020-09-10 21:12:06 +0530

    Brain part of Reactive Forms in Angular

--- signup-reactive/src/app/app.component.ts
+++ signup-reactive/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'signup-reactive';
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      acceptTandC: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if (this.registerForm.invalid){
+      return;
+    }
+    console.table(this.registerForm.value);
+    console.table(this.registerForm);
+    alert("SignUp Success\n" + JSON.stringify(this.registerForm.value));
+  }
+  
+  onReset(){
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
 }
