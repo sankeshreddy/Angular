0c6684fac4c42cab9788c41e822283a7c6199345
sankeshreddy <sankeshreddy@gmail.com>
2020-09-12 21:49:45 +0530

    added HttpClientModule,ToastrModule,BrowserAnimationsModule and FontAwesomeModule

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//http
+import {HttpClientModule} from '@angular/common/http';
+//toastr
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//forntAwesome
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 
@@ -10,7 +16,11 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
