41b9387251e3b3cd31933c48fcfcd926f56c8a36
sankeshreddy <sankeshreddy@gmail.com>
2020-09-14 21:17:45 +0530

    Business logic for generating user details

--- random-card/src/app/app.component.ts
+++ random-card/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './services/user.service';
+// import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'random-card';
+
+  user : any;
+
+  constructor(private userService: UserService, 
+    private toastr: ToastrService, ){}
+    // private location: Location
+
+  ngOnInit() {
+    
+    this.userService.getUser().subscribe(
+      (user:any) => {
+        console.log(user);
+        this.user = user.results[0];
+      },
+      (err) => {
+        console.log(err);
+        this.toastr.error(err.status, "OOPS Something went wrong");
+      },
+    );
+  }
+  
+  generateUser(){
+
+    this.userService.getUser().subscribe(
+      (user:any) => {
+        console.log(user);
+        this.user = user.results[0];
+    },
+    (err) => {
+      console.log(err);
+      this.toastr.error(err.status, "OOPS Something");
+    }
+    );
+  }
+
+  // load(){
+  //   location.reload();
+  // }
 }
