24823eb0f163986604cf78731e0cab463a3b5067
sankeshreddy <sankeshreddy@gmail.com>
2020-09-19 14:02:08 +0530

    service to fetch Github users

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
+// https://api.github.com/users/sankeshreddy
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails(userName: string){
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+
+  getRepos(repoUrl: string){
+    return this.http.get(repoUrl);
+  }
 }
